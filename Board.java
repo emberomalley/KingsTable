6cf6a2f8afc5f9f9630125e62516b7abe766d845
njbrown1997 <nbrown9@uvm.edu>
2019-04-09 23:12:41 -0400

    Adding piece capturing.

--- Board.java
+++ Board.java
@@ -1,23 +1,24 @@
 import java.util.ArrayList;
 import java.lang.Math;
 
-
 public class Board {
+
     //Fields.
     private int size = 11; //Dimensions of the board.
     private int tileSize = 45; //Size of tile of the board in pixels.
     public int[][] boardState = new int[size][size]; //Change to be an array of pieces later/
     private int selectedTileX = -1; //Selected tile of the board.
     private int selectedTileY = -1;
+
     //Constructor
     public Board() {
         //Set up the empty board.
         //These are positions for the 11x11 board. Modify later to depend on board dimensions.
-        for (int i=3;i<=7;i++){
+        for (int i = 3; i <= 7; i++) {
             boardState[i][0] = 2;
             boardState[0][i] = 2;
             boardState[10][i] = 2;
-            boardState[i][10] = 2;  
+            boardState[i][10] = 2;
         }
         boardState[5][1] = 2;
         boardState[1][5] = 2;
@@ -39,33 +40,30 @@ public class Board {
         //King
         boardState[5][5] = 3;
     }
-    
+
     //This method checks for a valid move and updates the board state.
     //Returns false if the move is invalid.
-    public boolean movePiece(int x1,int y1,int x2,int y2){
+    public boolean movePiece(int x1, int y1, int x2, int y2) {
         //Checks if the space is occupied.
-        if (boardState[x2][y2] != 0){
+        if (boardState[x2][y2] != 0) {
             return false;
-        }
-        //Checks for rook movement.
-        else if ((x1 != x2) && (y1 != y2)){
+        } //Checks for rook movement.
+        else if ((x1 != x2) && (y1 != y2)) {
             return false;
-        }
-        //Checks for pieces in the way.
-        else if (x1 != x2){
-            int l = Math.min(x1,x2);
-            int u = Math.max(x1,x2);
-            for (int i = l+1; i < u;i++){
-                if (boardState[i][y1] != 0){
+        } //Checks for pieces in the way.
+        else if (x1 != x2) {
+            int l = Math.min(x1, x2);
+            int u = Math.max(x1, x2);
+            for (int i = l + 1; i < u; i++) {
+                if (boardState[i][y1] != 0) {
                     return false;
                 }
             }
-        }
-        else if (y1 != y2){
-            int l = Math.min(y1,y2);
-            int u = Math.max(y1,y2);
-            for (int i = l+1; i < u;i++){
-                if (boardState[x1][i] != 0){
+        } else if (y1 != y2) {
+            int l = Math.min(y1, y2);
+            int u = Math.max(y1, y2);
+            for (int i = l + 1; i < u; i++) {
+                if (boardState[x1][i] != 0) {
                     return false;
                 }
             }
@@ -76,45 +74,88 @@ public class Board {
         boardState[x1][y1] = 0;
         return true;
     }
-    
+
+    //This method checks if the piece at position (x,y) is sandwiching any pieces
+    //of the opposite color. Returns true or false and updates the boardState.
+    public boolean checkCapture(int x, int y, String direction) {
+        if (direction == "right") {
+            if (y + 2 >= size) {
+                return false;
+            } else if (Math.abs(boardState[x][y] - boardState[x][y + 1]) == 1 && Math.abs(boardState[x][y] - boardState[x][y + 2]) != 1 && boardState[x][y + 1] != 0 && boardState[x][y + 2] != 0) {
+                System.out.println("Piece " + boardState[x][y + 1] + " captured from (" + x + "," + (y+1) + ").");
+                boardState[x][y + 1] = 0;
+                return true;
+            }
+        } else if (direction == "left") {
+            if (y - 2 < 0) {
+                return false;
+            } else if (Math.abs(boardState[x][y] - boardState[x][y - 1]) == 1 && Math.abs(boardState[x][y] - boardState[x][y - 2]) != 1 && boardState[x][y - 1] != 0 && boardState[x][y -2] != 0) {
+                System.out.println("Piece " + boardState[x][y - 1] + " captured from (" + x + "," + (y-1) + ").");
+                boardState[x][y - 1] = 0;
+                return true;
+            }
+        } else if (direction == "down") {
+            if (x + 2 >= size) {
+                return false;
+            } else if (Math.abs(boardState[x][y] - boardState[x + 1][y]) == 1 && Math.abs(boardState[x + 2][y] - boardState[x][y]) != 1 && boardState[x+1][y] != 0 && boardState[x+2][y] != 0) {
+                System.out.println("Piece " + boardState[x + 1][y] + " captured from (" + (x+1) + "," + y + ").");
+                boardState[x + 1][y] = 0;
+                return true;
+            }
+        } else if (direction == "up") {
+            if (x - 2 < 0) {
+                return false;
+            } else if (Math.abs(boardState[x][y] - boardState[x - 1][y]) == 1 && Math.abs(boardState[x - 2][y] - boardState[x][y]) != 1 && boardState[x-1][y] != 0 && boardState[x-2][y] != 0) {
+                System.out.println("Piece " + boardState[x - 1][y] + " captured from (" + (x-1) + "," + y + ").");
+                boardState[x - 1][y] = 0;
+                return true;
+            }
+        }
+        return false;
+    }
+
     //Getters and Setters.
     //Set and get the size of the board.
-    public void setSize(int size){
+    public void setSize(int size) {
         this.size = size;
     }
-    
-    public int getSize(){
+
+    public int getSize() {
         return this.size;
     }
-    
+
     //Set and get the size of a tile.
-    public void setTileSize(int tileSize){
+    public void setTileSize(int tileSize) {
         this.tileSize = tileSize;
     }
-    public int getTileSize(){
+
+    public int getTileSize() {
         return this.tileSize;
     }
-    
-    public int getSelectedTileX(){
+
+    public int getSelectedTileX() {
         return this.selectedTileX;
     }
-    public int getSelectedTileY(){
+
+    public int getSelectedTileY() {
         return this.selectedTileY;
     }
+
     public int getPieceType(int i, int j) {
-    	return boardState[i][j];
+        return boardState[i][j];
     }
-    public void printBoard(){
-        for (int i=0;i<size;i++){
-            for (int j=0;j<size;j++){
+
+    public void printBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 System.out.print(boardState[i][j]);
             }
             System.out.println();
         }
     }
-    
+
     public void tests() {
-    	
+
     }
-    
+
 }
